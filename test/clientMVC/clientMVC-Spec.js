07063b1540f4498f346c369ec0aade900efee6d9
MacKentoch <erwan.datin@gmail.com>
2015-07-05 21:31:48 +0200

    U-test : added removeThisLine() tests

--- test/clientMVC/clientMVC-Spec.js
+++ test/clientMVC/clientMVC-Spec.js
@@ -68,7 +68,7 @@ describe('ngwfWfEditController', function() {
         }));
 
 
-
+        //tets case configuration model has not been initialized
         it('should have no line', function(){
           $scope.configuration = {};
           expect($scope.configuration.lines).toBeUndefined();
@@ -82,9 +82,53 @@ describe('ngwfWfEditController', function() {
             $scope.addNewline();            
             expect($scope.configuration.lines.length).toEqual(2);
         });
-
-
       });
+
+      describe('removeThisLine', function() {
+
+        //reset scope.configuation before each 
+        //inject formFieldManage service to initialize configuration model
+        beforeEach(inject(function(_formFieldManage_){
+            formFieldManage = _formFieldManage_;
+            $scope.configuration = {};
+            formFieldManage.initConfigurationEditFromScratch($scope.configuration);          
+        }));
+
+        it('should not remove last line', function(){
+         $scope.removeThisLine(); 
+          expect($scope.configuration.lines.length).toEqual(1);
+        });
+
+        it('should have 2 lines left (had 3 lines - removed at index 0)', function() { 
+            $scope.addNewline();
+            $scope.addNewline(); 
+            $scope.removeThisLine(0);
+            expect($scope.configuration.lines.length).toEqual(2);
+        });
+
+        it('should still have 3 lines (index to remove > max index)', function() { 
+            $scope.addNewline();
+            $scope.addNewline(); 
+            $scope.removeThisLine(6);
+            expect($scope.configuration.lines.length).toEqual(3);
+        });
+
+        it('should still have 3 lines (index to remove < 0)', function() { 
+            $scope.addNewline();
+            $scope.addNewline(); 
+            $scope.removeThisLine(-1);
+            expect($scope.configuration.lines.length).toEqual(3);
+        });
+
+        //to test : index = index active line -> should change active line to fist line
+        it('should set activeLine = 1 since removed first line (and not the last one)', function() { 
+            $scope.addNewline();
+            $scope.addNewline(); 
+            $scope.removeThisLine(0);
+            expect($scope.configuration.activeLine).toEqual(1);
+        });
+
+      });      
 
 
 
