3a2ebe1cf6a61b3d1b16f97efb8c7f32954a291e
MacKentoch <erwan.datin@gmail.com>
2015-07-15 22:21:20 +0200

    fo on rebase

--- public/clientMVC/dragDrop/directives/ngwfDdDecorContainerDirective.js
+++ public/clientMVC/dragDrop/directives/ngwfDdDecorContainerDirective.js
@@ -13,9 +13,10 @@ var ngwfDdDecorContainerDirective = angular.module('ngwfApp.directives.ngwfDdDec
 ngwfDdDecorContainerDirective.directive('ddDecorContainer', [function(){
         var htmlTemplate   = [
                                 '<div ng-click="styleParam.collapse = !styleParam.collapse">- collapse -<div>',
-                                '<div collapse="styleParam.collapse" class="{{}}">', 
+                                '<div class="{{}}">', 
                                 ' <h5>{{currentTitle}}</h5>', 
-                                '</div>'].join(' ');
+                                '</div>',
+                                ''].join(' ');
 
         return {
             scope:  {
@@ -67,6 +68,7 @@ ngwfDdDecorContainerDirective.directive('ddDecorContainer', [function(){
                             // var isCollapsed = $scope.isCollapsed;
                             // $scope.returnCollapse = $scope.isCollapsed;
                             // console.info('isCollapse : ' + isCollapsed);
+                            //var isCollapsed = $scope.collapse;
 
                         }
                     }                    
@@ -77,11 +79,30 @@ ngwfDdDecorContainerDirective.directive('ddDecorContainer', [function(){
                 //prevent transclusion creating child scope 
                 //want to know more about what I'm saying : check this nice tip on the subject :
                 //http://angular-tips.com/blog/2014/03/transclusion-and-scopes/        
-                transclude($scope.$parent, function(clone, $scope){
-                    element.append(clone);
+                transclude($scope.$parent, function(contentClone){
+                    //element.append(contentClone);
+
+                    angular.forEach(contentClone, function(node){
+                        if (isDestinationContainer(node)) {
+                            element.append(node);
+                        }
+                    });
                 });   
 
 
             }
         };
+
+        function isDestinationContainer(node){
+            var yesItIs = false;
+            console.dir(node);
+            if (node.tagName &&  (
+                    node.hasAttribute('dd-decor-include-container-here') ||
+                    node.tagName.toLowerCase() === 'dd-decor-include-container-here')) {
+                console.info('found!!!');
+                yesItIs = true;
+            }
+            return yesItIs;
+        }
+
     }]);
