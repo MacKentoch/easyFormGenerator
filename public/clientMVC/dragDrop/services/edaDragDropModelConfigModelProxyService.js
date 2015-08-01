98132956cd2d3613504ad804b62d328aa35d7b00
MacKentoch <erwan.datin@gmail.com>
2015-08-01 07:30:49 +0200

    still on model bindings in ddModelConfModelProxyService

--- public/clientMVC/dragDrop/services/edaDragDropModelConfigModelProxyService.js
+++ public/clientMVC/dragDrop/services/edaDragDropModelConfigModelProxyService.js
@@ -20,7 +20,7 @@ angular
 
 
 			/**
-			 * return a control model 
+			 * return a control model that is more formly detailed
 			 * (more formly detailed : see controls property in EasyFormGenFormlyBindingModels._easyFormListControls)
 			 */
 			function getFormlyDetailedContorlModelFromDragDropObject(dragDrapCtrlModel){
@@ -28,12 +28,75 @@ angular
 				var listControl = EasyFormGenFormlyBindingModels.getEasyFormListControls();
 				var controlsListGetter = $parse('controls');
 
-				angular.forEach(controlsListGetter(listControl), function(ctrlListValue, ctrlListId){
+				angular.forEach(controlsListGetter(listControl), function(ctrlListValue){
 					if (ctrlListValue.id === dragDrapCtrlModel.control)  controlModel = ctrlListValue;
 				});
 
 				return controlModel;
 			}
+
+
+
+			/**
+			 * bind formly detailed model to configuration control model
+			 */
+			function returnConfigCtrlModelFromFormlyDetailedCtrlModel(formlyDetailCtrlModel, configurationCtrlModel){
+				      
+	     
+
+	      // var extractedProps = returnControlFromAddCtrlModalModel(modalAddCtrlModel);
+
+	      // configurationObj.lines[indexLine].columns[numcolumn].control.selectedControl 	= extractedProps.selectedControl;
+	      // configurationObj.lines[indexLine].columns[numcolumn].control.type 						= extractedProps.formlyType;
+	      // configurationObj.lines[indexLine].columns[numcolumn].control.subtype 					= extractedProps.formlySubtype;
+	      // /**
+	      //  * templateOptions
+	      //  */
+	      // configurationObj.lines[indexLine].columns[numcolumn].control.templateOptions = {
+	      //                                                                                   label				: '',
+	      //                                                                                   required		: false,
+	      //                                                                                   description	: '',
+	      //                                                                                   placeholder	: '',
+	      //                                                                                   options			: []
+	      //                                                                                 };
+	      //  /**
+	      //   * then bind template option
+	      //   */
+	      // configurationObj.lines[indexLine].columns[numcolumn].control.templateOptions.label 				= extractedProps.formlyLabel;
+	      // configurationObj.lines[indexLine].columns[numcolumn].control.templateOptions.required 		= extractedProps.formlyRequired;
+	      // configurationObj.lines[indexLine].columns[numcolumn].control.templateOptions.description 	= extractedProps.formlyDesciption;
+	      // configurationObj.lines[indexLine].columns[numcolumn].control.templateOptions.placeholder 	= extractedProps.formlyPlaceholder;
+	      // configurationObj.lines[indexLine].columns[numcolumn].control.templateOptions.options 			= extractedProps.formlyOptions;
+
+	      // /**
+	      //  * add additionnal — particular — properties :
+	      //  * 
+	      //  * -> datepicker : datepickerPopup
+	      //  */
+	      // if (configurationObj.lines[indexLine].columns[numcolumn].control.type === 'datepicker') {
+	      //  	configurationObj.lines[indexLine].columns[numcolumn].control.templateOptions.datepickerPopup = extractedProps.datepickerPopup;
+	      // }	
+
+	      // /**
+	      //  * unique key (set only first time) in this model is formly control type + Date.now(); 
+	      //  */
+	      // var newKey = configurationObj.lines[indexLine].columns[numcolumn].control.type + '-' + Date.now();
+
+	      // if (validKeyUniqueness(newKey, configurationObj) === true){
+	      //   configurationObj.lines[indexLine].columns[numcolumn].control.key = newKey;
+	      // }else{
+	      //   newKey = configurationObj.lines[indexLine].columns[numcolumn].control.type + '-' + Date.now();
+	      //   if (validKeyUniqueness(newKey, configurationObj) === true){
+	      //     configurationObj.lines[indexLine].columns[numcolumn].control.key = newKey;
+	      //   }else{
+	      //     newKey = configurationObj.lines[indexLine].columns[numcolumn].control.type + '-' + Date.now();
+	      //   }
+	      // }                                                                     
+
+	      // configurationObj.lines[indexLine].columns[numcolumn].control.edited = true;
+
+			}
+
 
 			var Service = {};
 
@@ -73,7 +136,8 @@ angular
 														'linevalue : ' 	: lineValue,
 														'colIndex'			: i,
 														'colValue : ' 	: lineValue[i],
-														'formlyDetailedModel' : getFormlyDetailedContorlModelFromDragDropObject(lineValue[i])
+														'formlyDetailedModel' : getFormlyDetailedContorlModelFromDragDropObject(lineValue[i]),
+														//'configModelCtrl' : controllerModalProxy.
 													}
 												);
 						
