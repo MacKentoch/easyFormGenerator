e6b7e0492def6e05e81f984a122da1641131f01f
MacKentoch <erwan.datin@gmail.com>
2015-08-08 17:14:27 +0200

    email required validation message now occur only if required is set

--- public/clientMVC/main/services/ngwfEditCtrlControllerModalProxy.js
+++ public/clientMVC/main/services/ngwfEditCtrlControllerModalProxy.js
@@ -129,9 +129,12 @@ angular
 																													              		//return a required validation message : 
 																													              		//-> '<label as name> is required '
 																													              		//-> or if not exists or empty just 'this field is required'
+																													              		
 																													              		var defaultReturnMsg = 'this Email field is required';
 																													              		var returnMsg = (typeof scope.to.label !== 'undefined') ? ((scope.to.label !== '') ? scope.to.label + ' is required' : defaultReturnMsg) : defaultReturnMsg;
-																													              		return returnMsg;
+																													              		//check if validation is really dued to require validation 
+																													              		//and not another validation like emailShape validator
+																													              		if (scope.to.required) return returnMsg;
 																													            		}
 						                                												}
 		                                										}
