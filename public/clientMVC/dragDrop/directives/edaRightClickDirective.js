ca35a2af8a7b0fcb2fe696bf8ed2b13db7b529bd
MacKentoch <erwan.datin@gmail.com>
2015-08-13 05:29:39 +0200

    edaRightClick : watch selectedState

--- public/clientMVC/dragDrop/directives/edaRightClickDirective.js
+++ public/clientMVC/dragDrop/directives/edaRightClickDirective.js
@@ -16,35 +16,54 @@ angular
     	function($parse){
     	
     	return {
+
+            scope: {
+                selectedState : '=edaIsSelected'
+            },
+
     		restrict: 'A',
     		 
     		link: function(scope, element, attrs) {
 	    		var fn             = $parse(attrs.edaRightClick);
                 var columnIndex    = $parse(attrs.edaRightClickColIndex);
                 var currentClass   = attrs.edaSelectedClass;
-                var selectedState  = $parse(attrs.edaIsSelected);
+                //var selectedState  = $parse(attrs.edaIsSelected);
 
 
-    	        element.bind('contextmenu', function(event) {
+
+    	        element.on('contextmenu', function(event) {
     	            scope.$apply(function() {
     	                event.preventDefault();
 
-                        console.info('selectedState=' + selectedState +'\n' + 'currentClass: ' +currentClass);
-
+                    
                         //right click limited to template column (index = 1)                           
                         if (columnIndex(scope) === 1) {
                             element.parent().removeClass(currentClass);
 
-                            if (selectedState) element.parent().addClass(currentClass);
-                            if (!selectedState) element.parent().removeClass(currentClass);
+                            if (scope.selectedState) element.parent().addClass(currentClass);
+                            if (!scope.selectedState) element.parent().removeClass(currentClass);
 
-                            selectedState = !selectedState;
+                            scope.selectedState = !scope.selectedState;
+
+                            console.info('changed state : ' + scope.selectedState);
                         }
 
                         //right click limited to template column (index = 1)   
     	                if (columnIndex(scope) === 1) fn(scope, {$event:event});
     	            });
     	        });	
+
+
+                // scope.$watch(function(){ return attrs.edaIsSelected; }, function (value){
+                //     console.log(value);
+                // });
+
+                scope.$watch(attrs.edaIsSelected, function (value){
+                    console.log(value);
+                });                
+                // attrs.$observe('', function(value){
+                //     console.warn(value);
+                // });
 
 
                 // function toggleSelected(val){
