b5f082a8e492754d0ac90db7afcb1d02b1e7e26d
MacKentoch <erwan.datin@gmail.com>
2015-08-14 05:42:13 +0200

    edaRightClick : will no more take care class on selection

--- public/clientMVC/dragDrop/directives/edaRightClickDirective.js
+++ public/clientMVC/dragDrop/directives/edaRightClickDirective.js
@@ -18,24 +18,19 @@ angular
     	
     	return {
 
-            // scope: {
-            //     selectedState : '=edaIsSelected'
-            // },
-
     		restrict: 'A',
     		 
     		link: function(scope, element, attrs) {
 	    		var fn                  = $parse(attrs.edaRightClick);
                 var columnIndex         = $parse(attrs.edaRightClickColIndex);
-                var currentClass        = attrs.edaSelectedClass;
-                var selectedState       = $parse(attrs.edaIsSelected);
+                //var currentClass        = attrs.edaSelectedClass;
+                //var selectedState       = $parse(attrs.edaIsSelected);
                 var fctSetRightclicked  = $parse(attrs.edaSetRightClicked);
                 
                 /**
                  * on right click event manage
                  * - open edit panel through attrs.edaRightClick function
                  * - set rightCliked attribute (to true) to control (in dragDropModel)
-                 * - add selected cssClass
                  */
     	        element.on('contextmenu', function(event) {
     	            scope.$apply(function() {
@@ -45,7 +40,6 @@ angular
                         //right click limited to template column (index = 1)                           
                         if (columnIndex(scope) === 1) {
                             
-
                             //element.parent().removeClass(currentClass);
 
                             //if (!selectedState(scope)) element.parent().addClass(currentClass);
@@ -60,46 +54,7 @@ angular
     	            });
     	        });	
 
-                /**
-                 * observe IsSelected (= rightClicked attribute)
-                 * - add/remove selected cssClass depending rightClicked attribute
-                 */
-                // attrs.$observe('edaIsSelected', function (newValue, oldValue){
-                    
 
-                //     if (columnIndex(scope) === 1 &&
-                //         newValue !== oldValue) {
-
-                //         console.info('observed :' + newValue);
-                //         console.dir(    
-                //                     {
-                //                         'elementParent' : element.parent(),
-                //                         'currentClass' : currentClass
-                //                     }
-                //                     );
-
-                //         element.parent().removeClass(currentClass);
-
-                //         //if (newValue === true) element.parent().addClass(currentClass);                        
-                //         //if (!value) element.parent().removeClass(currentClass);
-                //         if (!selectedState(scope)) element.parent().addClass(currentClass);
-                //         if (selectedState(scope)) element.parent().removeClass(currentClass);                        
-
-                //     }                       
-                    
-                // });                
-
-                scope.$watch(function(){return selectedState(scope);}, function (value){
-                    
-                    element.parent().removeClass(currentClass);
-
-                    if (value === true) element.parent().addClass(currentClass);
-                    if (value === false) element.parent().removeClass(currentClass);
-                    
-                    console.log('watch : ' + value);    
-                    
-                });                
-          
 
     		}
     	};
