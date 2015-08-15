0a3902247c0ac7b37b9c4db826f3bba592913165
MacKentoch <erwan.datin@gmail.com>
2015-08-15 07:07:36 +0200

    added Subtitle and texinput side panel edit content

--- public/clientMVC/dragDrop/edaApp.js
+++ public/clientMVC/dragDrop/edaApp.js
@@ -72,7 +72,7 @@ angular
 													'	            <h5 class="greyText"><i class="fa fa-pencil-square-o"></i>&nbsp; Edit :</h5>',
 													'	        </div>',
 													'	    </div> ',
-													'	    <hr/>',
+													//'	    <hr/>',
 
 													'	    <div class="row">',
 													'	        <div class="col-sm-12">',
@@ -99,16 +99,20 @@ angular
 													'            <h5 class="greyText"><i class="fa fa-pencil-square-o"></i>&nbsp; Edit properties :</h5>',
 													'        </div>',
 													'    </div> ',
-													'    <hr/>',
+													//'    <hr/>',
 
 													'    <div class="marginTopFivepixels"></div>',
 
 													'    <div class="row">',
 													'            <div class="form-group">',
-													'              <label for="inputTextDescriptionUpdate" class="col-lg-3 control-label greyText editPropertiesLabel">Header text :</label>',
-													'              <div class="col-lg-9">',
+													'							<div class="col-md-12">',
+
+													'              <label for="inputTextDescriptionUpdate" class="control-label greyText editPropertiesLabel">Header text :</label>',
+													'              <div class="">',
 													'                <input type="text" class="form-control" ng-model="panel.proxyModel.temporyConfig.formlyDesciption" id="inputHeaderTextUpdate" placeholder="Add / edit header text here">',
 													'              </div>',
+
+													'							</div>',
 													'            </div>',
 													'    </div>',
 
@@ -116,6 +120,144 @@ angular
 													validEditFooter,
 													'</div> '
   											].join(''));
+
+	 		/**
+			 * register template cache for side edit control panel
+			 * 
+			 * control : "SubTitle"
+			 */
+			$templateCache.put('editPanelSubTitleCtrl-tpls.html', 
+												[
+													'<div class="panel panel-default">',
+													'    <div class="panel-body">',
+
+													'    <div class="row">',
+													'        <div class="col-md-12">',
+													'            <h5 class="greyText"><i class="fa fa-pencil-square-o"></i>&nbsp; Edit properties :</h5>',
+													'        </div>',
+													'    </div> ',
+													//'    <hr/>',
+
+													'    <div class="marginTopFivepixels"></div>',
+
+													'    <div class="row">',
+													'            <div class="form-group">',
+
+													'							<div class="col-md-12">',
+
+													'              <label for="inputTextDescriptionUpdate" class="control-label greyText editPropertiesLabel">Subtitle text :</label>',
+													'              <div class="">',
+													'                <input type="text" class="form-control" ng-model="panel.proxyModel.temporyConfig.formlyDesciption" id="inputSubtitleTextUpdate" placeholder="Add / edit subtitle text here">',
+													'              </div>',
+
+													'							</div>',
+
+													'            </div>',
+													'    </div>',
+
+													'  </div>',
+													validEditFooter,
+													'</div> '
+												].join(''));
+
+
+	 		/**
+			 * register template cache for side edit control panel
+			 * 
+			 * control : "TextInput"
+			 */
+			$templateCache.put('editPanelTextInputCtrl-tpls.html', 
+												[
+													'<div class="panel panel-default">',
+													'    <div class="panel-body">',
+
+													'    <div class="row">',
+													'        <div class="col-md-12">',
+													'            <h5 class="greyText"><i class="fa fa-pencil-square-o"></i>&nbsp; Edit properties :</h5>',
+													'        </div>',
+													'    </div> ',
+													//'    <hr/>',
+
+													'    <div class="marginTopFivepixels"></div>',
+
+													//label text
+													'    <div class="row">',
+													'            <div class="form-group">',
+
+													'							<div class="col-md-12">',
+
+		                      '              <label for="inputTextLabelUpdate" class=" control-label greyText editPropertiesLabel">Label text :</label>',
+		                      '              <div class="">',
+		                      '                <input type="text" class="form-control" ng-model="nyaSelect.temporyConfig.formlyLabel" id="inputTextLabelUpdate" placeholder="Add / edit control label here">',
+		                      '              </div>',
+
+													'							</div>',
+
+													'            </div>',
+													'    </div>',
+
+
+													//placeholder
+													'    <div class="row">',
+													'            <div class="form-group">',
+
+													'							<div class="col-md-12">',
+
+	                        '              <label for="inputTextplaceholderUpdate" class="control-label greyText editPropertiesLabel">placeholder :</label>',
+	                        '              <div class="">',
+	                        '                <input type="text" class="form-control" ng-model="nyaSelect.temporyConfig.formlyPlaceholder" id="inputTextplaceholderUpdate" placeholder="Add / edit placeholder text here">',
+	                        '              </div>',
+
+													'							</div>',
+
+													'            </div>',
+													'    </div>',													
+
+
+													//required
+													'    <div class="row">',
+													'            <div class="form-group">',
+
+													'							<div class="">',
+
+	                        '              <label for="inputTextRequiredUpdate" class="col-md-4 control-label greyText editPropertiesLabel">Required :</label>',
+	                        '              <div class="col-md-8">',
+	                        '                <div class="checkboxCssCorrection">&nbsp;</div>',
+	                        '                <input type="checkbox" ng-model="nyaSelect.temporyConfig.formlyRequired" id="inputTextRequiredUpdate">',
+	                        '              </div>',
+
+													'							</div>',
+
+													'            </div>',
+													'    </div>',	
+
+
+													//description
+													'    <div class="row">',
+													'            <div class="form-group">',
+
+													'							<div class="col-md-12">',
+
+	                        '              <label for="inputTextDescriptionUpdate" class="control-label greyText editPropertiesLabel">Description :</label>',
+	                        '              <div class="">',
+	                        '                <input type="text" class="form-control" ng-model="nyaSelect.temporyConfig.formlyDesciption" id="inputTextDescriptionUpdate" placeholder="Add / edit description here">',
+	                        '              </div>',
+
+													'							</div>',
+
+													'            </div>',
+													'    </div>',	
+
+
+													'  </div>',
+													validEditFooter,
+													'</div> '
+												].join(''));
+
+
+
+
+
   	}
   ]);
 
