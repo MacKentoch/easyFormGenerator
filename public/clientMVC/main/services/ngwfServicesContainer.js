b18e90719205512b541e7b6a7068ac6d8aafe5ee
MacKentoch <erwan.datin@gmail.com>
2015-08-20 05:40:17 +0200

    step way rewrite.

--- public/clientMVC/main/services/ngwfServicesContainer.js
+++ public/clientMVC/main/services/ngwfServicesContainer.js
@@ -1,17 +1,27 @@
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// module = "services" container
-//  ------------------------------------------------------
-//      Syntax (convention) :
-//          "ngwfApp" = application
-//          "ngwfApp.services" = container services module
-//
-//  This module is a container -> it must be injected in the application -> so it will inject all services injected here
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-angular.module('ngwfApp.services', [	'ngwfApp.services.formFieldManage',
-										'ngwfApp.services.selectOptionManage',
-										'ngwfApp.services.ngwfWfFormsServices',
-										'ngwfApp.services.ngwfEditCtrlControllerModalProxy',
-										function () {
-    //verbose
-    //console.log('--> INIT : Hello services container  \'\'ngwfApp.services\'\' ');
-}]);
+/**
+ *  ------------------------------------------------------
+ *  module = "services" container
+ *  ------------------------------------------------------
+ *
+ * contains all app services
+ *
+ * ——————————————————————————————————————————————
+ * MIT (2015) - Erwan Datin (MacKentoch)
+ * https://github.com/MacKentoch/easyFormGenerator
+ * ——————————————————————————————————————————————
+**/
+
+(function () {
+	'use strict';
+
+	angular.module(
+									'ngwfApp.services', [	
+																				'ngwfApp.services.formFieldManage',
+																				'ngwfApp.services.selectOptionManage',
+																				'ngwfApp.services.ngwfWfFormsServices',
+																				'ngwfApp.services.ngwfEditCtrlControllerModalProxy'
+																			]
+								);
+
+})(); 
+
--- public/clientMVC/main/services/ngwfWfFormsServices.js
+++ public/clientMVC/main/services/ngwfWfFormsServices.js
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//  form API : suppose you have your RESTful backend 
+//  
 //
 //  module = "service"  for view "wfEdit"
 //  ------------------------------------------------------
@@ -9,6 +9,21 @@
 //
 //  This module is a service -> it must be injected in services container
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///
+///
+///
+/**
+ *  ------------------------------------------------------
+ *  form API : suppose you have your RESTful backend 
+ *  ------------------------------------------------------
+ *
+ *  
+ *
+ * ——————————————————————————————————————————————
+ * MIT (2015) - Erwan Datin (MacKentoch)
+ * https://github.com/MacKentoch/easyFormGenerator
+ * ——————————————————————————————————————————————
+**/
 var ngwfWfFormsServices = angular.module('ngwfApp.services.ngwfWfFormsServices', ['ngResource']);
 
 
