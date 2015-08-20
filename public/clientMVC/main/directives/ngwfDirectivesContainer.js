e79e80ffb91445c38d56d98a64acc9cb3eeef5c2
MacKentoch <erwan.datin@gmail.com>
2015-08-20 21:42:17 +0200

    stratio directive rewritten

--- public/clientMVC/main/directives/ngwfDirectivesContainer.js
+++ public/clientMVC/main/directives/ngwfDirectivesContainer.js
@@ -7,8 +7,7 @@
 //
 //  This module is a container -> it must be injected in the application -> so it will inject all directives injected here
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-angular.module('ngwfApp.directives', [	'ngwfApp.directives.ngwfStRationDirective',
+angular.module('ngwfApp.directives', [	'ngwfApp.directives.ngwfStRatioDirective',
 										function () {
-    //verbose
-    //console.log('--> INIT : Hello directives container  \'\'ngwfApp.directives\'\' ');
+
 }]);
--- public/clientMVC/main/directives/ngwfStRatioDirective.js
+++ public/clientMVC/main/directives/ngwfStRatioDirective.js
@@ -1,27 +1,41 @@
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// module = "directive" 
-//  ------------------------------------------------------
-//      Syntax (convention) :
-//          "ngwfApp" = application
-//          "ngwfApp.directives.directiveNAME" = container directives module
-//
-//  This module is a directive -> it must be injected in directives container
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-var ngwfStRatioDirective = angular.module('ngwfApp.directives.ngwfStRationDirective', []);
+/**
+ *  ------------------------------------------------------
+ *  simple directive to set width style attribute in %
+ *  ------------------------------------------------------
+ *
+ *  
+ *
+ * ——————————————————————————————————————————————
+ * MIT (2015) - Erwan Datin (MacKentoch)
+ * https://github.com/MacKentoch/easyFormGenerator
+ * ——————————————————————————————————————————————
+**/
+
+(function () {
+	
+	'use strict';
+
+	angular
+		.module('ngwfApp.directives.ngwfStRatioDirective', [])
+		.directive('stRatio', stRatio);
+
+		stRatio.$inject = [];
+		function stRatio(){
+
+			var directive = {
+				link : linkfct
+			};
+
+			return directive;
+
+			function linkfct(scope, element, attr){
+				var ratio=+(attr.stRatio);
+			  element.css('width',ratio+'%');
+			}
+
+		}
+
+})(); 
 
 
-//this directive is attribute only : add "st-ration = "__A NUMBER__" in view or html :
-ngwfStRatioDirective.directive('stRatio',[function(){
 
-	console.log('--> INIT : Hello directive  \'\'stRatio\'\' ');
-
-        return {
-
-          link:function(scope, element, attr){
-            var ratio=+(attr.stRatio);
-            
-            element.css('width',ratio+'%');
-            
-          }
-        };
-}]);
