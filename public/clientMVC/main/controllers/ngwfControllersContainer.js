5ef8c1cdc9bfad8f7a0a6c0d14f99243a0add636
MacKentoch <erwan.datin@gmail.com>
2015-08-21 05:07:36 +0200

    rewritting

--- public/clientMVC/main/controllers/ngwfControllersContainer.js
+++ public/clientMVC/main/controllers/ngwfControllersContainer.js
@@ -1,16 +1,24 @@
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// module = "controllers" container
-//  ------------------------------------------------------
-//      Syntax (convention) :
-//          "ngwfApp" = application
-//          "ngwfApp.controllers" = container controller module
-//
-//  This module is a container -> it must be injected in the application -> so it will inject all controllers injected here
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-angular.module('ngwfApp.controllers', [	'ngwfApp.controllers.ngwfMainController',
-										'ngwfApp.controllers.ngwfWfEditController',
-										'ngwfApp.controllers.ngwfWfEditMODALController',							
-										function () {
-    //verbose
-    //console.log('--> INIT : Hello controllers container  \'\'ngwfApp.controllers\'\' ');
-}]);
+/**
+ *  ------------------------------------------------------
+ *  controllers container
+ *  ------------------------------------------------------
+ *
+ * 
+ * ——————————————————————————————————————————————
+ * MIT (2015) - Erwan Datin (MacKentoch)
+ * https://github.com/MacKentoch/easyFormGenerator
+ * ——————————————————————————————————————————————
+**/
+(function () {
+	'use strict';
+
+
+	angular.module('ngwfApp.controllers', [	
+																				'ngwfApp.controllers.ngwfMainController',
+																				'ngwfApp.controllers.ngwfWfEditController',
+																				'ngwfApp.controllers.ngwfWfEditMODALController'
+																				]
+								);
+
+})(); 
+
