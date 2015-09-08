d78464c2d73ac8172e1a79a056d0db0a3613c266
MacKentoch <erwan.datin@gmail.com>
2015-09-08 05:20:50 +0200

    step way : WfFormsByIdServices cleaned

--- public/js/eda.stepway.js
+++ public/js/eda.stepway.js
@@ -3728,34 +3728,6 @@ $templateCache.put("editModalTemplate.html","<div class=modal-header><h3 class=\
 
 /**
  *  ------------------------------------------------------
- *  form API : suppose you have your RESTful backend 
- *  ------------------------------------------------------
- *
- *  
- *
- * ——————————————————————————————————————————————
- * MIT (2015) - Erwan Datin (MacKentoch)
- * https://github.com/MacKentoch/easyFormGenerator
- * ——————————————————————————————————————————————
-**/
-
-(function(){
-
-	'use strict';
-	
-	angular
-		.module('ngwfApp.services.ngwfWfFormsServices', ['ngResource'])
-		.factory('WfFormsByIdServices', WfFormsByIdServices);
-
-		WfFormsByIdServices.$inject = ['$resource'];
-		function WfFormsByIdServices($resource){
-			return $resource('/api/wfedit/:id', {id: '@id'}, {});
-		}
-
-
-})();
-/**
- *  ------------------------------------------------------
  *  filters container
  *  ------------------------------------------------------
  *
