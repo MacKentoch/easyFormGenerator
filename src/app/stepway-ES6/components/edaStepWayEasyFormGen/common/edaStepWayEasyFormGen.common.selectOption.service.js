c800b9ff6f82568acc356f9f5738388b41f70349
MacKentoch <erwan.datin@gmail.com>
2015-10-05 13:29:20 +0200

    feat : ES6 - selectOptionManage service

---
+++ src/app/stepway-ES6/components/edaStepWayEasyFormGen/common/edaStepWayEasyFormGen.common.selectOption.service.js
@@ -0,0 +1,236 @@
+/* global angular */
+
+class selectOptionManage {
+	
+	constructor() {
+		
+	}
+	
+	
+	initModel(selectObj){
+		this.resetModel(selectObj);
+	}	
+
+
+	resetModel(selectObj){
+		let zeroModel = { rows:[] };
+		angular.copy(zeroModel, selectObj);
+	}	
+
+	
+	isOptionUnique(selectObj, textValue){
+		for (let i = selectObj.rows.length - 1; i >= 0; i--) {
+			if (selectObj.rows[i].option === textValue) return false;
+		}
+		return true;
+	}	
+
+	
+	isOptionValidFormat(textValue){
+		if (textValue !== '')  return true;
+		return false;
+	}	
+	
+	addNewOptionRadio(selectObj, newOptionText){
+		let fullResponse = {
+					resultFlag 	: false,
+					details 		: ''
+				};
+		let checkResult = this.validOption(selectObj, newOptionText);  
+		if (checkResult.resultFlag === true){
+	
+			let newOption = {
+						option	: newOptionText,
+						order		: selectObj.rows.length
+					};
+	
+			selectObj.rows.push(newOption);
+			fullResponse.resultFlag = true;
+			fullResponse.details = '';
+			return fullResponse;
+			
+		}else{
+			angular.copy(checkResult, fullResponse);                    
+			return fullResponse;                        
+		}
+	}
+	
+
+	addNewOptionBasicSelect(selectObj, newOptionText){
+			var fullResponse = {
+						resultFlag 	: false,
+						details 		: ''
+					};
+	
+			let checkResult = this.validOption(selectObj, newOptionText);  
+			if (checkResult.resultFlag === true){
+	
+				let newOption = {
+							option	: newOptionText,
+							order		: selectObj.rows.length
+						};
+	
+				selectObj.rows.push(newOption);
+				fullResponse.resultFlag = true;
+				fullResponse.details 		= '';
+				
+				return fullResponse;
+			}else{
+				angular.copy(checkResult, fullResponse);                    
+				return fullResponse;                        
+			}
+	}
+	
+	
+	addNewOptionGroupedSelect(selectObj, newOptionText, newOptionGroup){
+		let fullResponse = {
+					resultFlag 	: false,
+					details 		: ''
+				};
+	
+		let checkResult = this.validOption(selectObj, newOptionText);  
+	
+		if (checkResult.resultFlag === true){
+	
+			let newOption = {
+						option	: newOptionText,
+						group		: newOptionGroup,
+						order		: selectObj.rows.length
+					};
+	
+			selectObj.rows.push(newOption);
+			fullResponse.resultFlag = true;
+			fullResponse.details = '';
+			return fullResponse;
+				
+		}else{
+					angular.copy(checkResult, fullResponse);                    
+					return fullResponse;                        
+		}
+	}			
+
+
+	removeOption(selectObj, AtIndex) {
+		let fullResponse = {
+					resultFlag : false,
+					details : ''
+				};
+	
+		if (AtIndex !== -1) {
+			selectObj.rows.splice(AtIndex, 1);
+			fullResponse.resultFlag = true;
+			fullResponse.details= '';
+			return fullResponse;
+		}else{
+			fullResponse.resultFlag = false;
+			fullResponse.details= 'Option index not valid';
+			return fullResponse;
+		}
+	}
+
+
+
+	upthisOption(selectObj, indexOption){
+		var fullResponse = {
+					resultFlag : false,
+					details : ''
+				};  
+	
+		if (indexOption > -1) {
+			if (indexOption > 0) {
+				if (selectObj.rows[indexOption - 1]) {
+					let currentOption = selectObj.rows[indexOption];
+					selectObj.rows.splice(indexOption , 1);
+					selectObj.rows.splice((indexOption - 1), 0, currentOption); 
+					fullResponse.resultFlag = true;
+					fullResponse.details 		= '';
+					return fullResponse;
+				}else{
+					fullResponse.resultFlag = false;
+					fullResponse.details 		= `Can't retreive option from option index`;
+					return fullResponse;
+				}
+			}else{
+					fullResponse.resultFlag = true;
+					fullResponse.details 		= '';
+					return fullResponse;
+			}  
+		}else{
+			fullResponse.resultFlag = false;
+			fullResponse.details 		= 'Option index not valid';
+			return fullResponse;
+		}
+	}
+	
+
+	downthisOption(selectObj, indexOption){
+		let fullResponse = {
+					resultFlag : false,
+					details : ''
+				};
+	
+		if (indexOption > -1) {
+			if (indexOption < selectObj.rows.length - 1){
+				if (selectObj.rows[indexOption + 1]) {
+					let currentOption = selectObj.rows[indexOption];
+					selectObj.rows.splice(indexOption , 1);
+					selectObj.rows.splice((indexOption + 1), 0, currentOption);  
+					fullResponse.resultFlag = true;
+					fullResponse.details 		= '';
+					return fullResponse;  
+				}else{
+					fullResponse.resultFlag = false;
+					fullResponse.details 		= `Can't retreive option from option index`;
+					return fullResponse;
+				}
+			}else{ 
+					fullResponse.resultFlag = true;
+					fullResponse.details 		= '';
+				return fullResponse;
+			}
+		}else{
+			fullResponse.resultFlag = false;
+			fullResponse.details = 'Option index not valid';
+			return fullResponse;
+		}
+	}
+	
+
+
+	validOption(selectObj, newOptionText){
+			let fullResponse = {
+						resultFlag  : false,
+						details     : ''
+					};
+	
+			if (typeof newOptionText === 'undefined') {
+				fullResponse.resultFlag = false;
+				fullResponse.details    = 'Entered option is empty';
+				return fullResponse;
+			}
+	
+			if (newOptionText !== '') {
+				for (let i = selectObj.rows.length - 1; i >= 0; i--) {
+					if (selectObj.rows[i].option === newOptionText) {
+						fullResponse.resultFlag = false;
+						fullResponse.details    = 'Entered option is not unique';
+						return fullResponse;
+					}
+				}
+				fullResponse.resultFlag = true;
+				fullResponse.details    = '';
+				return fullResponse;
+			}
+			fullResponse.resultFlag = false;     
+			fullResponse.details    = 'Entered option is empty';
+			return fullResponse;
+	}
+	
+	
+}
+
+
+selectOptionManage.$inject = [];
+
+export default selectOptionManage;
+
--- src/app/stepway-ES6/components/edaStepWayEasyFormGen/config/edaStepWayEasyFormGen.conf.formly.js
+++ src/app/stepway-ES6/components/edaStepWayEasyFormGen/config/edaStepWayEasyFormGen.conf.formly.js
@@ -1,3 +1,4 @@
+/* global angular */
 import {
 	richTextTemplate,
 	blankTemplate,
