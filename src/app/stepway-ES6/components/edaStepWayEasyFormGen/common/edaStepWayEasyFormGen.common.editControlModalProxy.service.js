80dc1b661cbfe739e9440630b7c9580faa34844e
MacKentoch <erwan.datin@gmail.com>
2015-10-12 05:23:31 +0200

    chore : ES6 - editControlModalProxy

---
+++ src/app/stepway-ES6/components/edaStepWayEasyFormGen/common/edaStepWayEasyFormGen.common.editControlModalProxy.service.js
@@ -0,0 +1,14 @@
+import editCtrlModalProxyHelper from './edaStepWayEasyFormGen.common.editControlModalProxy.helpers';
+
+class controllerModalProxy{
+	
+	constructor() {
+		
+	}
+	
+	
+	
+}
+
+controllerModalProxy.$inject= [];
+export default controllerModalProxy;
