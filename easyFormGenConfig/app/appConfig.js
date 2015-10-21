25f1b0e91dedd34575308d6fca96d1f897d6c5ed
MacKentoch <erwan.datin@gmail.com>
2015-10-21 22:03:16 +0200

    chore : update version

--- easyFormGenConfig/app/appConfig.js
+++ easyFormGenConfig/app/appConfig.js
@@ -12,8 +12,8 @@ module.exports = {
 	
 	//easy form generator verioning
 	version : {
-		stepWay 				: '1.0.28',
-		dragAndDropWay 	: '1.0.28'
+		stepWay 				: '1.0.29',
+		dragAndDropWay 	: '1.0.29'
 	},
 	
 	concatVendorFiles : false
