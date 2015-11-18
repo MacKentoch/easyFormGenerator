fcb2cdbd8baa60486a9555024a205aec59b5ff9c
MacKentoch <erwan.datin@gmail.com>
2015-11-18 05:14:07 +0100

    chore : ES6 -drag drop way

--- src/app/dragdropway-ES6/components/common/rightclick/edaDragDropWay.rightClick.module.js
+++ src/app/dragdropway-ES6/components/common/rightclick/edaDragDropWay.rightClick.module.js
@@ -3,9 +3,15 @@ import edaRightClick, {
 	EDA_RIGHT_CLICK_DIRECTIVE
 }														from './edaDragDropWay.rightClick.directive';
 
+import ddItemRightClickedManager, {
+	EDA_RIGHT_CLICKED_MANAGER_SERVICE
+}														from './edaDragDropWay.rightClick.service';
+
+
 const EDA_RIGHT_CLICK_MODULE = 'eda.right.click.module';
 
 
 export default angular
 								.module(EDA_RIGHT_CLICK_MODULE, [])
-								.directive(EDA_RIGHT_CLICK_DIRECTIVE, edaRightClick);
+								.directive(EDA_RIGHT_CLICK_DIRECTIVE, edaRightClick)
+								.service(EDA_RIGHT_CLICKED_MANAGER_SERVICE, ddItemRightClickedManager);
---
+++ src/app/dragdropway-ES6/components/common/rightclick/edaDragDropWay.rightClick.service.js
@@ -0,0 +1,47 @@
+/* global angular */
+const EDA_RIGHT_CLICKED_MANAGER_SERVICE = 'ddItemRightClickedManager';
+
+class ddItemRightClickedManager{
+	constructor(){
+		this.init();	
+	}
+	
+	init(){
+		
+	}
+	
+	/**
+		* setUnRightClicked 
+		* set unSelected (see edaRightClick directive)
+		*
+		* used in edaEditcontroller when droping control
+		*/	
+	setUnRightClicked(dragDropModelItem){
+		dragDropModelItem.rightCliked = false;
+	}	
+
+	/**
+		* resetAllDragDropItemSelectedState
+		*
+		* simply reset (=set to false) all item.rightCliked 
+		* in edit column (dragable column)
+		*
+		* used in edaEditPanel when closeEditPanel() called
+		*/	
+	resetAllDragDropItemSelectedState(dragDropModel){
+		angular.forEach(dragDropModel[1] ,(line)=>{
+			angular.forEach(line, (item)=>item.rightCliked = false);
+		});
+	
+	}	
+	
+	
+}
+
+ddItemRightClickedManager.$inject = [];
+
+export default ddItemRightClickedManager;
+
+export {
+	EDA_RIGHT_CLICKED_MANAGER_SERVICE
+};
