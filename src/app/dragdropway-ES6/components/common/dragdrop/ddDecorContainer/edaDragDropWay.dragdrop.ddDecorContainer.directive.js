f3e9642f7245b1f76300f6c11d45e8fcadf2f146
MacKentoch <erwan.datin@gmail.com>
2015-11-30 05:55:21 +0100

    chore : ES6 drag and drop

--- src/app/dragdropway-ES6/components/common/dragdrop/ddDecorContainer/edaDragDropWay.dragdrop.ddDecorContainer.directive.js
+++ src/app/dragdropway-ES6/components/common/dragdrop/ddDecorContainer/edaDragDropWay.dragdrop.ddDecorContainer.directive.js
@@ -1,3 +1,4 @@
+/* global angular */
 import ddDecorContainerTemplate 			from './edaDragDropWay.dragdrop.ddDecorContainer.template!text';
 import ddDecorContainerController, {
 	DD_DECOR_CONTAINER_CONTROLLER_NAME,
@@ -28,7 +29,78 @@ function ddDecorContainer(){
 	return directive;
 	
 	function linkFct($scope, element, attrs, ctrl, transclude){
-		//TODO : to finish migration - take care NEW : bindToController and controllerAs syntax
+		let verboseModeActive   											= $scope.ddDecorContainerCtrl.verboseMode;
+		let currentIndex        											= $scope.ddDecorContainerCtrl.currentIndex;
+		$scope.ddDecorContainerCtrl.isCollapsed				= false;		
+		$scope.ddDecorContainerCtrl.config.isEnabled	= false;
+		
+		/**
+			* forceCollapse when : 
+			*  dragDropConfigModel.containerConfig.decoration.isCollapsed changed (here bound to $scope.isStillCollapsed)
+			*/
+		$scope.$watch(()=> $scope.ddDecorContainerCtrl.isStillCollapsed,(newVal, oldVal)=>{
+			if (newVal !== oldVal) {
+				if ($scope.$parent.$parent.$index === 0) $scope.ddDecorContainerCtrl.isCollapsed = newVal;
+			}
+		});		
+		
+		
+		/**
+			* verbose mode for developments only
+			*/
+		if (verboseModeActive !== '') {
+			var verbose = angular.lowercase(verboseModeActive);
+			if (verbose === 'true' || verbose === '1') {
+					console.dir({
+						whoAmI              :   'I am verbose from ddDecorContainer link',
+						verbodeMode         :   verbose,
+						ParentParentIndex   :   $scope.$parent.$parent.$index,
+						ParentIndex         :   $scope.$parent.$index,
+						currentIndex        :   currentIndex,
+						styleParam          :   $scope.ddDecorContainerCtrl.styleParam,
+						columnindex         :   $scope.$parent.$parent.$parent.$parent.$index
+					});
+			}                    
+		}
+				
+		/**
+			* no header (no title, no collapse....) 
+			*/
+		//$scope.ddDecorContainerCtrl.config.isEnabled
+		if (typeof currentIndex !== 'undefined') {
+			if (currentIndex !== '') {
+				/**
+					* specific 1st column 
+					*/
+				if (currentIndex === '0') {
+					/**
+						* apply title  
+						*/
+					if (typeof $scope.ddDecorContainerCtrl.styleParam.title !== 'undefined') {
+							$scope.currentTitle     											= $scope.ddDecorContainerCtrl.styleParam.title;
+							$scope.ddDecorContainerCtrl.config.isEnabled	= true;
+							$scope.ddDecorContainerCtrl.isCollapsed     	= true;
+					} 
+				}
+			}                    
+		}	
+		
+		
+		/**
+			* prevent transclusion creating child scope 
+			* want to know more about what I'm talking about : check this nice tip on the subject :
+			* http://angular-tips.com/blog/2014/03/transclusion-and-scopes/        
+			*/
+		transclude($scope.$parent, function(contentClone){
+				/**
+					* transclusion will append content to '<div id="ddDecorContainerWillTranscludeHere"></div>' 
+					*/
+				var childDiv = angular.element(element.children()[1]); 
+				childDiv.append(contentClone);
+		});  			
+		
+				
+				
 	}
 	
 }
