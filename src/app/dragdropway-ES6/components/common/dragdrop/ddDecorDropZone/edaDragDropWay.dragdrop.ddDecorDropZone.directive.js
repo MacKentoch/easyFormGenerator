3d8240226e8cc67802a370bf7e14330bfee7c9b2
MacKentoch <erwan.datin@gmail.com>
2015-12-01 05:22:41 +0100

    chore : ES6 drag and drop

---
+++ src/app/dragdropway-ES6/components/common/dragdrop/ddDecorDropZone/edaDragDropWay.dragdrop.ddDecorDropZone.directive.js
@@ -0,0 +1,68 @@
+/* global angular */
+import ddDecorDropZoneTemplate from './edaDragDropWay.dragdrop.ddDecorDropZone.template.html!text';
+
+const DD_DECOR_DROPZONE_DIRECTIVE = 'ddDecorDropZone';
+
+function ddDecorDropZone(){
+	let directive = {
+		restrict 	: 'A',
+		template 	: ddDecorDropZoneTemplate,
+		scope			:	{
+			'styleParam'    : '=ddDropZoneProperties',
+			'verboseMode'   : '@ddDropZoneVerboseMode',
+			'currentIndex'  : '@ddDropZoneCurrentIndex',
+			'addNewLineFct' : '&ddDropZoneAddNewLine'			
+		},
+		transclude	: true,
+		link				: linkfct 
+	};
+	return directive;
+	
+	function linkfct($scope){
+
+		let verboseModeActive   = $scope.verboseMode;
+		let currentIndex        = $scope.currentIndex;
+		
+		$scope.headerConfig = {
+			HeaderButtonVisible : false,
+			affixAttr           : 'bs-affix',
+			affixEnabled        : false
+		};
+		/**
+			* verbose mode : just for dev
+			*/
+		if (verboseModeActive !== '') {
+			let verbose = angular.lowercase(verboseModeActive);
+			if (verbose === 'true' || verbose === '1') {
+				console.dir({
+					whoAmI              : 'I am verbose from ddDecorDropZone link',
+					verbodeMode         : verbose,
+					ParentParentIndex   : $scope.$parent.$parent.$index,
+					ParentIndex         : $scope.$parent.$index,
+					currentIndex        : currentIndex,
+					styleParam          : $scope.styleParam
+				});
+			}                    
+		}
+	
+	
+		if (typeof currentIndex !== 'undefined') {
+			if (currentIndex !== '') {
+				// apply title
+				if (typeof $scope.styleParam.title !== 'undefined') $scope.currentTitle = $scope.styleParam.title;
+				//apply font-awesome
+				if (typeof $scope.styleParam.fontAwesomeIcon !== 'undefined') $scope.currentFontAwesome = $scope.styleParam.fontAwesomeIcon;
+				//show add new line button
+				if (currentIndex === '1') $scope.headerConfig.HeaderButtonVisible = true;
+			}                    
+		}		
+		
+	}
+	
+}
+
+export default ddDecorDropZone;
+
+export {
+	DD_DECOR_DROPZONE_DIRECTIVE
+};
--- src/app/dragdropway-ES6/components/common/dragdrop/edaDragDropWay.dragdrop.module.js
+++ src/app/dragdropway-ES6/components/common/dragdrop/edaDragDropWay.dragdrop.module.js
@@ -8,12 +8,16 @@ import ddDecorContainerDirective, {
 import ddDecorContainerController, {
 	DD_DECOR_CONTAINER_CONTROLLER_NAME
 } 																		from './ddDecorContainer/edaDragDropWay.dragdrop.ddDecorContainer.controller';
-
+import ddDecorDropZone, {
+	DD_DECOR_DROPZONE_DIRECTIVE
+} 																		from './/ddDecorDropZone/edaDragDropWay.dragdrop.ddDecorDropZone.directive';
 
 const DRAGDROP_MODULE = 'edaDragDropWay.dragdrop.module';
+
 
 export default angular
 								.module(DRAGDROP_MODULE, [])
 								.directive(DD_CONTENT_COUNTER_DIRECTIVE, ddContentCounter)
 								.controller(DD_DECOR_CONTAINER_CONTROLLER_NAME, ddDecorContainerController)
-								.directive(DD_DECOR_CONTAINER_DIRECTIVE, ddDecorContainerDirective);
+								.directive(DD_DECOR_CONTAINER_DIRECTIVE, ddDecorContainerDirective)
+								.directive(DD_DECOR_DROPZONE_DIRECTIVE, ddDecorDropZone);
