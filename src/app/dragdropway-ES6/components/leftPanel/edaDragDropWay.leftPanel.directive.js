3b9f7340cd8a8c79598b87cba134aebf75a9147c
MacKentoch <erwan.datin@gmail.com>
2015-12-10 05:14:19 +0100

    chore : ES6 - leftPanel component

---
+++ src/app/dragdropway-ES6/components/leftPanel/edaDragDropWay.leftPanel.directive.js
@@ -0,0 +1,31 @@
+import {
+	LEFT_PANEL_CONTROLLER,
+	LEFT_PANEL_CONTROLLERAS
+}																from './edaDragDropWay.leftPanel.controller';
+
+import leftPanelTemplate from '';
+
+const LEFT_PANEL_DIRECTIVE = 'leftPanel';
+
+
+function leftPanel(){
+	let directive = {
+		restrict 					: 'E',
+		template					: leftPanelTemplate,
+		scope							: {},
+		controller				:	LEFT_PANEL_CONTROLLER,
+		controllerAs			: LEFT_PANEL_CONTROLLERAS,
+		bindToController	: {
+			closeEditPanel : '&'
+		} 
+	};
+	return directive;
+}
+
+leftPanel.$inject = [];
+
+export default leftPanel;
+
+export {
+	LEFT_PANEL_DIRECTIVE
+};
--- src/app/dragdropway-ES6/components/leftPanel/edaDragDropWay.leftPanel.module.js
+++ src/app/dragdropway-ES6/components/leftPanel/edaDragDropWay.leftPanel.module.js
@@ -2,11 +2,14 @@
 import leftPanelController, {
 	LEFT_PANEL_CONTROLLER
 }														from './edaDragDropWay.leftPanel.controller';
-
+import leftPanel, {
+	LEFT_PANEL_DIRECTIVE
+}														from './edaDragDropWay.leftPanel.directive';
 
 const LEFT_PANEL_MODULE = 'edaDragDropWay.leftPanel.module';
 
 
 export default angular
 								.module(LEFT_PANEL_MODULE, [])
+								.directive(LEFT_PANEL_DIRECTIVE, leftPanel)
 								.controller(LEFT_PANEL_CONTROLLER, leftPanelController);
