29b4a886efb6ff3929fc84fbbde5434ae725a7ba
MacKentoch <erwan.datin@gmail.com>
2016-01-05 05:09:57 +0100

    chore : ES6 drag and drop

---
+++ src/app/dragdropway-ES6/components/formlyProxy/edaDragDropWay.formlyProxy.formFieldManage.provider.helpers.js
@@ -0,0 +1,133 @@
+ export const initEasyFormListControls = () => {
+  return {											
+    controls        : [],
+    selectedControl : 'none' ,
+    temporyConfig   : {
+      selectedControl		: 'none',
+      formlyLabel				: 'label',  
+      formlyRequired		: false, 
+      formlyDesciption	: '',
+      formlyPlaceholder	: '',
+      formlyOptions 		: []
+    } 
+  };
+};
+  
+export const initEasyFormEmptyConfigurationLineModel = () => {
+  return {
+    line 					: 1,                                       
+    activeColumn 	: 1,
+    columns 			: [{  
+      numColumn 	: 1,
+      exist 			:true, 
+      control 		: {
+        type:'none',
+        key: 'none'
+      }
+    }]
+  };      
+}
+  
+export const initEmptyControlFieldModel = () => {
+  return {
+    'control' 		: {
+      'type'  : 'none',
+      'key'   : 'none',
+    }	
+  };  
+};
+  
+export const initEasyFormInitialStateConfigurationModel = (_easyFormEmptyConfigurationLineModel) => {
+  // commun all easy form generator ways
+  return {                                
+    submitButtonText 	: 'submit',
+    cancelButtonText 	: 'cancel',
+    lines : [].concat(_easyFormEmptyConfigurationLineModel)
+  };    
+};
+
+export const initEasyFormInitialStateConfigurationModelAddOnForStepWay = () => {
+  // specific easy form generator — step way — (not drag and drop way), needed for wizard management
+  return {
+    activeLine 			: 1,   
+    listConfigStep 	: [
+      'init',
+      'first',
+      'second',
+      'third'
+    ],
+    stepIndicators 	: [
+      true,
+      false,
+      false,
+      false
+    ], 
+    configStepCounter : 0,     	
+  };
+};
+  
+export const initEasyFormReloadConfigurationModel = () => {
+  let reloadConfigModel = initEasyFormInitialStateConfigurationModel();
+  reloadConfigModel.lines = [];
+  return reloadConfigModel;
+};
+  
+export const initHeaderTemplates = () => {
+  return {
+    cssClass 		: ['col-xs-12', 'col-xs-6', 'col-xs-4'],
+    textContent : '',
+    html_part1 	: [
+                    '  <div class="',
+                    ].join(''),
+    selectedClass : '',
+    html_part2 	: [
+                    '">',
+                    '    <h2 class="text-center">'																							
+                  ].join(''),							 
+    html_part3  : this.textContent,
+    html_part4 	:  [ 
+                    '    <h2>',
+                    '    <hr/>',
+                    '  </div>',
+                    ].join(''), 
+    simpleHtml1 : 	[
+                    '<h2 class="text-center">'
+                    ].join(''),
+    simpleHtml2 : 	[
+                    '    <h2>',
+                    '    <hr/>',																
+                    ].join('')						 
+  };    
+};
+  
+  
+export const initFormlyControlTemplates = () => {
+  return {
+    className : ['col-xs-12', 'col-xs-6', 'col-xs-4'],
+    type      : '',
+    key       : '',
+    templateOptions: {
+      type        : '',
+      label       : '',
+      required    : '',
+      placeholder : '',
+      description : '',
+      options     : ''    
+    }
+  };
+};
+  
+export const initParticularControlProperties = () => {
+  return [
+    {
+      controlType 	: 'datepicker',
+      properties 		: [	
+        {	
+        isRoot  					: false, 
+        isTemplateOptions : true, 
+        value 						: 'datepickerPopup'
+      }
+      ]
+    }
+  ];
+};
--- src/app/dragdropway-ES6/components/formlyProxy/edaDragDropWay.formlyProxy.formFieldManage.provider.js
+++ src/app/dragdropway-ES6/components/formlyProxy/edaDragDropWay.formlyProxy.formFieldManage.provider.js
@@ -1,153 +1,48 @@
+import * as helpers from './edaDragDropWay.formlyProxy.formFieldManage.provider.helpers';
+
 const EASY_FORM_FORMLY_BINDING_MODEL_PROVIDER = 'EasyFormGenFormlyBindingModels';
 
 function EasyFormGenFormlyBindingModels(){
 
-  let _easyFormListControls                                   =	initEasyFormListControls();
-  let _easyFormEmptyConfigurationLineModel                    = initEasyFormEmptyConfigurationLineModel();
-  let _emptyControlFieldModel                                 = initEmptyControlFieldModel();
-  let _easyFormInitialStateConfigurationModel                 = initEasyFormInitialStateConfigurationModel();
-  let _easyFormInitialStateConfigurationModelAddOnForStepWay  = initEasyFormInitialStateConfigurationModelAddOnForStepWay();
-  let _easyFormReloadConfigurationModel                       = initEasyFormReloadConfigurationModel();
-  let _headerTemplates                                        = initHeaderTemplates(); 
-  let _formlyControlTemplates                                 = initFormlyControlTemplates();  
-  let _particularControlProperties                            = initParticularControlProperties();  
+  let _easyFormListControls                                   =	helpers.initEasyFormListControls();
+  let _easyFormEmptyConfigurationLineModel                    = helpers.initEasyFormEmptyConfigurationLineModel();
+  let _emptyControlFieldModel                                 = helpers.initEmptyControlFieldModel();
+  let _easyFormInitialStateConfigurationModel                 = helpers.initEasyFormInitialStateConfigurationModel(_easyFormEmptyConfigurationLineModel);
+  let _easyFormInitialStateConfigurationModelAddOnForStepWay  = helpers.initEasyFormInitialStateConfigurationModelAddOnForStepWay();
+  let _easyFormReloadConfigurationModel                       = helpers.initEasyFormReloadConfigurationModel();
+  let _headerTemplates                                        = helpers.initHeaderTemplates(); 
+  let _formlyControlTemplates                                 = helpers.initFormlyControlTemplates();  
+  let _particularControlProperties                            = helpers.initParticularControlProperties();  
   
-    
-    
-    
-  function initEasyFormListControls(){
-    return {											
-      controls        : [],
-      selectedControl : 'none' ,
-      temporyConfig   : {
-        selectedControl		: 'none',
-        formlyLabel				: 'label',  
-        formlyRequired		: false, 
-        formlyDesciption	: '',
-        formlyPlaceholder	: '',
-        formlyOptions 		: []
-      } 
-    };
-  }
+  this.getAllParticularControlProperties                      = getAllParticularControlProperties;
+  this.addParticularControlProperties                         = addParticularControlProperties;
   
-  function initEasyFormEmptyConfigurationLineModel(){
-    return {
-    line 					: 1,                                       
-    activeColumn 	: 1,
-    columns 			: [{  
-      numColumn 	: 1,
-      exist 			:true, 
-      control 		: {
-        type:'none',
-        key: 'none'
+  function getAllParticularControlProperties(){
+    return _particularControlProperties;
+  }    
+    
+  function addParticularControlProperties(newParticularControlProperty){
+    // test object param has waited properties
+    if (('controlType' 	in newParticularControlProperty) &&
+        ('properties' 	in newParticularControlProperty)) {
+      let isAnUpdate = false;
+      if ( _particularControlProperties.length > 0 ) {
+        _particularControlProperties.forEach(function(controlProp){
+          if (controlProp.controlType === newParticularControlProperty.controlType) {
+            controlProp.properties = [].concat(newParticularControlProperty.properties);
+            isAnUpdate = true;
+          }	
+        });
       }
-    }]
-    };      
-  }
-  
-  function initEmptyControlFieldModel(){
-    return {
-      'control' 		: {
-        'type'  : 'none',
-        'key'   : 'none',
-      }	
-    };  
-  }
-  
-  function initEasyFormInitialStateConfigurationModel(){
-    // commun all easy form generator ways
-    return {                                
-      submitButtonText 	: 'submit',
-      cancelButtonText 	: 'cancel',
-      lines : [].concat(_easyFormEmptyConfigurationLineModel)
-    };    
-  }
+      if (!isAnUpdate) {
+        _particularControlProperties.push(newParticularControlProperty);
+      }
+    }
+    return _particularControlProperties;			
+  }  
 
-  function initEasyFormInitialStateConfigurationModelAddOnForStepWay(){
-    // specific easy form generator — step way — (not drag and drop way), needed for wizard management
-    return {
-	    activeLine 			: 1,   
-	    listConfigStep 	: [
-        'init',
-        'first',
-        'second',
-        'third'
-      ],
-	    stepIndicators 	: [
-        true,
-        false,
-        false,
-        false
-      ], 
-	    configStepCounter : 0,     	
-    };
-  }
-  
-  function initEasyFormReloadConfigurationModel(){
-    let reloadConfigModel = initEasyFormInitialStateConfigurationModel();
-    reloadConfigModel.lines = [];
-    return reloadConfigModel;
-  }
-  
-  function initHeaderTemplates(){
-    return {
-      cssClass 		: ['col-xs-12', 'col-xs-6', 'col-xs-4'],
-      textContent : '',
-      html_part1 	: [
-                      '  <div class="',
-                      ].join(''),
-      selectedClass : '',
-      html_part2 	: [
-                      '">',
-                      '    <h2 class="text-center">'																							
-                    ].join(''),							 
-      html_part3  : this.textContent,
-      html_part4 	:  [ 
-                      '    <h2>',
-                      '    <hr/>',
-                      '  </div>',
-                      ].join(''), 
-      simpleHtml1 : 	[
-                      '<h2 class="text-center">'
-                      ].join(''),
-      simpleHtml2 : 	[
-                      '    <h2>',
-                      '    <hr/>',																
-                      ].join('')						 
-    };    
-  }
-  
-  
-  function initFormlyControlTemplates(){
-    return {
-      className : ['col-xs-12', 'col-xs-6', 'col-xs-4'],
-      type      : '',
-      key       : '',
-      templateOptions: {
-        type        : '',
-        label       : '',
-        required    : '',
-        placeholder : '',
-        description : '',
-        options     : ''    
-      }
-    };
-  }
-  
-  function initParticularControlProperties(){
-    return [
-      {
-        controlType 	: 'datepicker',
-        properties 		: [	
-         {	
-          isRoot  					: false, 
-          isTemplateOptions : true, 
-          value 						: 'datepickerPopup'
-        }
-       ]
-     }
-    ];
-  }
+
+ 
 
 
 }
