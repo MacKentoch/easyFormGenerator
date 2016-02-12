72edb75ffdeeaae4711f80b77ec05de9ffd2373f
MacKentoch <erwan.datin@gmail.com>
2016-02-12 05:10:18 +0100

    chore : drag and drop ES6

---
+++ src/app/dragdropway-ES6/components/leftPanel/ValidEditFooter/edaDragDropWay.leftpanel.validEditFooter.directive.js
@@ -0,0 +1,19 @@
+import leftPanelValidEditFooterTemplate from './edaDragDropWay.leftpanel.validEditFooter.template.html!text';
+
+const LEFT_PANEL_VALID_EDIT_FOOTER_DIRECTIVE = 'leftPanelValidEditFooter';
+
+function leftPanelValidEditFooter() {
+  let directive = {
+    restrict : 'E',
+    template : leftPanelValidEditFooterTemplate
+  };
+  return directive;
+}
+
+leftPanelValidEditFooter.$inject = [];
+
+export default leftPanelValidEditFooter;
+
+export {
+  LEFT_PANEL_VALID_EDIT_FOOTER_DIRECTIVE
+};
