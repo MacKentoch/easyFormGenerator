dbb113d8aea5951d53e63d10c9676d7e18002ce1
MacKentoch <erwan.datin@gmail.com>
2016-02-16 05:08:09 +0100

    chore : drag and drop ES6

---
+++ src/app/dragdropway-ES6/components/leftPanel/DateControl/edaDragDropWay.leftpanel.dateControl.directive.js
@@ -0,0 +1,20 @@
+
+import leftPanelDateControlTemplate from './edaDragDropWay.leftpanel.dateControl.template.html!text';
+
+const LEFT_PANEL_DATE_CONTROL_DIRECTIVE = 'leftPanelDateControl';
+
+function leftPanelDateControl() {
+  let directive = {
+    restrict : 'E',
+    template : leftPanelDateControlTemplate
+  };
+  return directive;
+}
+
+leftPanelDateControl.$inject = [];
+
+export default leftPanelDateControl;
+
+export {
+  LEFT_PANEL_DATE_CONTROL_DIRECTIVE
+};
