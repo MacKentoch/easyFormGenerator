332a2aed6befd64fe0f46b55ab0b192013b41be4
MacKentoch <erwan.datin@gmail.com>
2016-02-21 06:04:53 +0100

    chore : drag and drop ES6

---
+++ src/app/dragdropway-ES6/components/leftPanel/BasicSelectControl/edaDragDropWay.leftpanel.basicSelectControl.directive.js
@@ -0,0 +1,20 @@
+
+import leftPanelBasicSelectControlTemplate from './edaDragDropWay.leftpanel.basicSelectControl.template.html!text';
+
+const LEFT_PANEL_BASIC_SELECT_CONTROL_DIRECTIVE = 'leftPanelBasicSelectControl';
+
+function leftPanelBasicSelectControl() {
+  let directive = {
+    restrict : 'E',
+    template : leftPanelBasicSelectControlTemplate
+  };
+  return directive;
+}
+
+leftPanelBasicSelectControl.$inject = [];
+
+export default leftPanelBasicSelectControl;
+
+export {
+  LEFT_PANEL_BASIC_SELECT_CONTROL_DIRECTIVE
+};
