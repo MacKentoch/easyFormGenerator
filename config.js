f857ea370bd17037f2c1e465e4b69c0985d83a4f
MacKentoch <erwan.datin@gmail.com>
2016-03-08 05:10:05 +0100

    chore : devDependencies as npm packages

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ System.config({
   },
 
   map: {
-    "angular": "src/app/ES6-adpters/angular",
+    "angular": "github:angular/bower-angular@1.5.0",
     "babel": "npm:babel-core@5.8.25",
     "babel-runtime": "npm:babel-runtime@5.8.24",
     "core-js": "npm:core-js@1.1.4",
