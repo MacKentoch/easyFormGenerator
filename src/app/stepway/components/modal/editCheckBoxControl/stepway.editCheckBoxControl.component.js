b2239386719a7bd3cb019f277f00c1e634359943
MacKentoch <erwan.datin@gmail.com>
2016-04-03 06:42:03 +0200

    chore : refactor

---
+++ src/app/stepway/components/modal/editCheckBoxControl/stepway.editCheckBoxControl.component.js
@@ -0,0 +1,20 @@
+import editCheckBoxControlTemplate from './stepway.editCheckBoxControl.template.html!text';
+
+export const EDIT_BASIC_SELECT_COMPONENT = 'editCheckBoxControl';
+
+export const editCheckBoxControlComponent = {
+  template      : editCheckBoxControlTemplate,
+  bindings      : {
+    nyaSelect:  '='
+  },
+  controller    :
+  class editCheckBoxControlController {
+    constructor() {
+
+    }
+
+    static get $inject() {
+      return [];
+    }
+  }
+};
