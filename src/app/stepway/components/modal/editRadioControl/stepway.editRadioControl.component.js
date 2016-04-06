61aa61f84c9f0def9a66c8b397529539526eff0c
MacKentoch <erwan.datin@gmail.com>
2016-04-06 05:08:50 +0200

    chore : refactor

---
+++ src/app/stepway/components/modal/editRadioControl/stepway.editRadioControl.component.js
@@ -0,0 +1,27 @@
+import editRadioControlTemplate from './stepway.editRadioControl.template.html!text';
+
+export const EDIT_RADIO_CONTROL_COMPONENT = 'editRadioControl';
+
+export const editRadioControlComponent = {
+  template      : editRadioControlTemplate,
+  bindings      : {
+    nyaSelect:          '=',
+    radioRowCollection: '=',
+    newOptionRadio:     '=',
+    addNewOptionRadio:  '&',
+    radioFilter:        '&',
+    upThisRadioRow:     '&',
+    downThisRadioRow:   '&',
+    removeRadioRow:     '&'
+  },
+  controller    :
+  class editRadioControlController {
+    constructor() {
+      //
+    }
+
+    static get $inject() {
+      return [];
+    }
+  }
+};
