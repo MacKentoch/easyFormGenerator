fa825d69b74f741cbe3dd0591aaa0cd7c4e20961
MacKentoch <erwan.datin@gmail.com>
2016-04-27 05:12:11 +0200

    chore : refactor

---
+++ src/app/stepway/components/controls/Email/config/stepway.email.config.js
@@ -0,0 +1,36 @@
+export const EmailConfig =  {
+	id 													: 'Email',
+	name 												: 'Email',
+	subtitle 										: 'Email',
+	group 											: 'input',
+	formlyType									: 'input',
+	formlySubtype 							: 'email',
+	formlyLabel 								: '',
+	formlyRequired 							: false,
+	formlyDesciption 						: '',
+	formlyOptions 							: [],
+	formlyExpressionProperties 	: {},
+	formlyValidators 						: {
+		emailShape : {
+			expression : (viewValue, modelValue) => {
+				var value = modelValue || viewValue;
+				return /^[a-z]+[a-z0-9._]+@[a-z]+\.[a-z.]{2,5}$/.test(value);
+			},
+			message		: '$viewValue + \' is not a valid email\''
+		}
+	},
+	formlyValidation: {
+		messages: {
+			required: function(viewValue, modelValue, scope) {
+				//return a required validation message :
+				//-> '<label as name> is required '
+				//-> or if not exists or empty just 'this field is required'
+				var defaultReturnMsg 	= 'this Email field is required';
+				var returnMsg 				= (typeof scope.to.label !== 'undefined') ? ((scope.to.label !== '') ? scope.to.label + ' is required' : defaultReturnMsg) : defaultReturnMsg;
+				//check if validation is really dued to require validation
+				//and not another validation like emailShape validator
+				if (scope.to.required) return returnMsg;
+			}
+		}
+	}
+};
--- src/app/stepway/components/controls/index.js
+++ src/app/stepway/components/controls/index.js
@@ -1,9 +1,11 @@
-import { IpAdressConfig } from './IpAdress/config';
-import { DateConfig }     from './Date/config';
-import { CheckBoxConfig } from './CheckBox/config';
+import { IpAdressConfig } from './IpAdress/config/stepway.IpAdress.config';
+import { DateConfig }     from './Date/config/stepway.date.config';
+import { CheckBoxConfig } from './CheckBox/config/stepway.checkbox.config';
+import { EmailConfig }    from './Email/config/stepway.email.config';
 
 export {
   IpAdressConfig,
   DateConfig,
-  CheckBoxConfig
+  CheckBoxConfig,
+  EmailConfig
 };
