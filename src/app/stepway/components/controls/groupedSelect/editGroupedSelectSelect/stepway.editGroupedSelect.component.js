879000c5174aee8e7889597bdb587ad941d6c971
MacKentoch <erwan.datin@gmail.com>
2016-05-12 05:28:15 +0200

    chore : refactor

---
+++ src/app/stepway/components/controls/groupedSelect/editGroupedSelectSelect/stepway.editGroupedSelect.component.js
@@ -0,0 +1,32 @@
+import editGroupedSelectControlTemplate from './stepway.editGroupedSelectControl.template.html!text';
+
+export const EDIT_GROUPED_SELECT_COMPONENT = 'editGroupedSelectControl';
+
+export const editGroupedSelectControlComponent = {
+  template      : editGroupedSelectControlTemplate,
+  bindings      : {
+    nyaSelect:                  '=',
+    groupedSelectRowCollection: '=',
+    newOptionGroupedSelect:     '=',
+    newGroupGroupedSelect:      '=',
+    groupSelectGroupClick:      '=',
+    GroupedSelectGroups:        '=',
+
+    addNewOptionGroupedSelect:  '&',
+    addNewGroupToGroupedSelect: '&',
+    upThisGroupedSelectRow:     '&',
+    downThisGroupedSelectRow:   '&',
+    showGroupListToChoose:      '&',
+    removeGroupedSelectRow:     '&'
+  },
+  controller    :
+  class editGroupedSelectControlController {
+    constructor() {
+
+    }
+
+    static get $inject() {
+      return [];
+    }
+  }
+};
--- src/app/stepway/components/controls/index.js
+++ src/app/stepway/components/controls/index.js
@@ -7,13 +7,17 @@ import { EmailConfig }        from './Email/config/stepway.email.config';
 import { BasicSelectConfig }  from './basicSelect/config/stepway.basicSelect.config';
 import { BlankConfig }        from './blank/config/stepway.blank.config';
 
+import { GroupedSelectConfig }  from './groupedSelect/config/stepway.groupedSelect.config';
+import {editGroupedSelectControlComponent, EDIT_GROUPED_SELECT_COMPONENT} from './groupedSelect/editgroupedSelect/stepway.editgroupedSelect.component';
+
 const configs = {
   IpAdressConfig,
   DateConfig,
   CheckBoxConfig,
   EmailConfig,
   BasicSelectConfig,
-  BlankConfig
+  BlankConfig,
+  GroupedSelectConfig
 };
 
 const controls = {
@@ -24,6 +28,10 @@ const controls = {
   editDate: {
     name:       EDIT_DATE_COMPONENT,
     component:  editDateControlComponent
+  },
+  editGroupedSelect: {
+    name: EDIT_GROUPED_SELECT_COMPONENT,
+    component: editGroupedSelectControlComponent
   }
 };
 
