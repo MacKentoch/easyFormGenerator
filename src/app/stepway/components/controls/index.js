99262cd13092fb7d165024223636cbf92c7a2c6e
MacKentoch <erwan.datin@gmail.com>
2017-01-16 05:11:27 +0100

    v2.1.4

--- src/app/stepway/components/controls/index.js
+++ src/app/stepway/components/controls/index.js
@@ -1,32 +1,31 @@
-import { BlankConfig }        from './blank/config/blank.config';
+import { BlankConfig } from './blank/config/blank.config';
 import editBlankModule, {editBlankControlComponent, EDIT_BLANK_CONTROL_COMPONENT} from './blank/editBlank/editBlankControl.component';
 
-import { IpAdressConfig }     from './IpAdress/config/IpAdress.config';
+import { IpAdressConfig } from './IpAdress/config/IpAdress.config';
 import editIpAdressModule, {editIpAdressControlComponent, EDIT_IP_ADRESS_COMPONENT} from './IpAdress/editIpAdress/editIpAdress.component';
 
-import { DateConfig }         from './Date/config/date.config';
+import { DateConfig } from './Date/config/date.config';
 import editDateModule, {editDateControlComponent, EDIT_DATE_COMPONENT} from './Date/editDate/editDate.component';
 
-import { CheckBoxConfig }     from './CheckBox/config/checkbox.config';
+import { CheckBoxConfig } from './CheckBox/config/checkbox.config';
 import editCheckBoxModule, {editCheckBoxControlComponent, EDIT_CHECKBOX_COMPONENT} from './CheckBox/editCheckBox/editCheckBox.component';
 
-import { EmailConfig }        from './Email/config/email.config';
+import { EmailConfig } from './Email/config/email.config';
 import editEmailControlModule, {editEmailControlComponent, EDIT_EMAIL_COMPONENT} from './Email/editEmail/editEmail.component';
 
-import { BasicSelectConfig }  from './basicSelect/config/basicSelect.config';
+import { BasicSelectConfig } from './basicSelect/config/basicSelect.config';
 import editBasicSelectModule, { editBasicSelectControlComponent, EDIT_BASIC_SELECT_COMPONENT } from './basicSelect/editBasicSelect/editBasicSelect.component';
 
-import { GroupedSelectConfig }  from './groupedSelect/config/groupedSelect.config';
+import { GroupedSelectConfig } from './groupedSelect/config/groupedSelect.config';
 import editGroupedSelectModule, {editGroupedSelectControlComponent, EDIT_GROUPED_SELECT_COMPONENT} from './groupedSelect/editGroupedSelect/editGroupedSelect.component';
 
-import { HeaderConfig }  from './header/config/header.config';
+import { HeaderConfig } from './header/config/header.config';
 import editHeaderControl, {editHeaderControlComponent, EDIT_HEADER_CONTROL_COMPONENT} from './header/editHeader/editHeaderControl.component';
 
-import { PasswordConfig }  from './password/config/stepway.password.config';
-import {editPasswordControlComponent, EDIT_PASSWORD_CONTROL_COMPONENT} from './password/editPassword/stepway.editPassword.component';
-import editPasswordModule from './password/editPassword/stepway.editPassword.module';
+import { PasswordConfig } from './password/config/password.config';
+import editPasswordModule, {editPasswordControlComponent, EDIT_PASSWORD_CONTROL_COMPONENT} from './password/editPassword/editPassword.component';
 
-import { RadioConfig }  from './radio/config/stepway.radio.config';
+import { RadioConfig } from './radio/config/stepway.radio.config';
 import {editRadioControlComponent, EDIT_RADIO_CONTROL_COMPONENT} from './radio/editRadio/stepway.editRadio.component';
 import editRadioModule from './radio/editRadio/stepway.editRadio.module';
 
